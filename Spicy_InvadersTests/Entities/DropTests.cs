5c50a6a94db37eeadad6be7ced7f576e0400a244
ethanschafstall <ethans9999@gmail.com>
2024-11-28 17:21:15 +0100

    test(drop tests): added drop test class
    Has 3 test methods for each of the UATs of the loot drop US

---
+++ Spicy_InvadersTests/Entities/DropTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spicy_Invaders;
+using Entity;
+
+namespace Spicy_InvadersTests.Entities
+{
+    [TestClass]
+    public class EnemySpawnerTests
+    {
+        [TestMethod]
+        public void MelonDropsLoot()
+        {
+            // Arrange
+            Enemy enemy = new Enemy(1 ,2 ,EnemyType.Melon);
+            List<Drop> drops = new List<Drop>();
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void LootFallsDown()
+        {
+        }
+        [TestMethod]
+        public void LootDropDisappears()
+        {
+        }
+    }
+}
