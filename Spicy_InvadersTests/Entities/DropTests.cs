8ad0ea4bcb50794efb7b951b83e3e33b5b07c134
ethanschafstall <ethans9999@gmail.com>
2024-12-05 14:02:55 +0100

    test(drop tests): Adds tests
    Adds the tests for LootFallsDown and LootDropDisappears

--- Spicy_InvadersTests/Entities/DropTests.cs
+++ Spicy_InvadersTests/Entities/DropTests.cs
@@ -12,25 +12,74 @@ namespace Spicy_InvadersTests.Entities
     [TestClass]
     public class EnemySpawnerTests
     {
+
         [TestMethod]
         public void MelonDropsLoot()
         {
             // Arrange
             Enemy enemy = new Enemy(1 ,2 ,EnemyType.Melon);
+            enemy.HealthPoints = 10;
             List<Drop> drops = new List<Drop>();
+            Bullet bullet = new Bullet();
+            bullet.Damage = 10;
+
 
             // Act
+            drops.Add(enemy.Hit(bullet));
 
             // Assert
-            Assert.AreEqual(1, 1);
+            Assert.AreEqual(drops.Count(), 1);
+
         }
+
         [TestMethod]
         public void LootFallsDown()
         {
+            // Arrange
+            GameEngine gameEngine = new GameEngine();
+            Game game = new Game(new Language.English(), (WeaponType)2, ConsoleColor.Gray, new List<ConsoleKey> { });
+
+            int baseXPos = 3;
+            int baseYPos = 3;
+            Drop drop = new Drop(baseXPos, baseYPos, dropType: DropType.weaponUpgrade);
+
+            Enemy enemy = new Enemy(10, 10, EnemyType.Melon);
+
+            // TODO : this list doesn't exist yet. 
+            //gameEngine.Drops.Add(drop);
+            game.GameLogic = gameEngine;
+
+            // Act 
+            game.Run();
+
+            // Assert
+            Assert.AreNotEqual(drop.Position.X, baseXPos);
         }
         [TestMethod]
         public void LootDropDisappears()
         {
+            // Arrange
+            GameEngine gameEngine = new GameEngine();
+            Game game = new Game(new Language.English(), (WeaponType)2, ConsoleColor.Gray, new List<ConsoleKey> { });
+
+            int baseXPos = 10;
+            int baseYPos = 10;
+            Drop drop = new Drop(baseXPos, baseYPos, dropType: DropType.weaponUpgrade);
+
+            Enemy enemy = new Enemy(3, 3, EnemyType.Melon);
+
+            // TODO : this list doesn't exist yet. 
+            //gameEngine.Drops.Add(drop);
+            game.GameLogic = gameEngine;
+
+            // Act 
+            game.Run();
+
+
+            // Assert
+            // TODO
+            //bool dropsListEmpty = game.GameLogic.Drops.Count() < 1;
+            //Assert.IsTrue(dropsListEmpty);
         }
     }
 }
